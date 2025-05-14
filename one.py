5e2977eeac04cdd7ab46243433ab405171ab4395
Waqar Ahmed <waqarshujrah@gmail.com>
2025-05-14 20:53:50 +0100

    here here

--- one.py
+++ one.py
@@ -101,5 +101,5 @@ def update(id):
 if __name__ == "__main__":
     with app.app_context():
          db.create_all()
-    app.run(host="0.0.0.0", port=8000, debug=false)
+    app.run(host="0.0.0.0", port=8000, debug=False)
 
